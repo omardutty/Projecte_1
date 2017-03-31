3b2b3cce965d697b35526bb7bdaec6e42bd138c8
OscarEspejoSagues <xxgreysoulxx@gmail.com>
2017-03-31 10:01:32 +0200

    Resubido

--- Assets/Scipts/PlayerEnemy.cs
+++ Assets/Scipts/PlayerEnemy.cs
@@ -13,6 +13,23 @@ public class PlayerEnemy : MonoBehaviour {
     private float movSpeed;
     private bool facingRight;
 
+    [SerializeField]
+    private Transform[] groundPoints;
+
+    [SerializeField]
+    private float growndRadius;
+
+    [SerializeField]
+    private LayerMask WhatIsGrownd;
+    private bool isGrownded;
+    private bool jump;
+
+    [SerializeField]
+    private bool airControl;
+
+    [SerializeField]
+    private float jumpForce;
+
     // Use this for initialization
     void Start () {
         facingRight = true;
@@ -21,32 +38,82 @@ public class PlayerEnemy : MonoBehaviour {
 
 
     }
-	
-	// Update is called once per frame
-	void FixedUpdate() { 
+
+    void Update()
+    {
+        HandleInput();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() { 
 
         float horizontal = Input.GetAxis("Horizontal");
- 
+
+        isGrownded = IsGrownded();
+
+        
 
         Moviments(horizontal);
-        Flip(horizontal);
-	}
+       Flip(horizontal);
+        
+
+    }
 
     private void Moviments(float horizontal) {
 
 
         myRigidbody.velocity = new Vector2(horizontal*movSpeed, myRigidbody.velocity.y);
+
         myAnimator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        if (isGrownded && jump)
+        {
+            isGrownded = false;
+            myRigidbody.AddForce(new Vector2(0, jumpForce));
+          
+            jump = false;
+        }
+        
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            jump = true; 
+        }
+
 
     }
     private void Flip(float horizontal)
     {
         if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
-        {
+         {
             facingRight = !facingRight;
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
         }
+    }
+
+
+    private bool IsGrownded()
+    {
+        if (myRigidbody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, growndRadius, WhatIsGrownd);
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+        }
+        return false;
     }
 }
